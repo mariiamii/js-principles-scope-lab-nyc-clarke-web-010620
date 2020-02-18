8a0d7e145f6d57c84cc2f46aff336000be145115
Mariam Anuashvili <m.anuashvili@gmail.com>
2020-02-18 02:26:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var customerName = 'bob';
 
 function upperCaseCustomerName(customerName) {
-  customerName.toUpperCase;
+  customerName.toUpperCase();
 }
 
   // describe('upperCaseCustomerName()', function() {
